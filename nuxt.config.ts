3a006aa725d94009a7cf979b409dae8cbb127bde
Eric Nguyen <enguyen807@gmail.com>
2023-12-13 10:23:26 +0200

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  // modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase'],
   css: ['/assets/main.css'],
+  supabase: {
+    redirect: false
+  },
+  typescript: {
+    shim: false
+  }
 })
